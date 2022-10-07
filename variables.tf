96eeb7f943a3a2b2d709a9e203194ee949d43a26
jhflo <jorgeflorez28@hotmail.com>
2022-10-07 10:01:54 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,42 @@
-variable "aws_key" {
-  type = string
-}
-
-variable "aws_secret" {
-  type = string
-}
-
 variable "aws_region" {
-  type        = string
   default     = "us-east-1"
+}
+
+variable "az_count" {
+  default     = "3"
+  description = "Number of availability zones in above region"
+}
+
+variable "ecs_task_execution_role" {
+  default     = "myECcsTaskExecutionRole"
+  description = "ECS task execution role name"
+}
+
+variable "app_image" {
+  default     = "jorfloba/timeoff-test:latest"
+  description = "Docker image to run in this ECS cluster"
+}
+
+variable "app_port" {
+  default     = "3000"
+  description = "Port on the docker image"
+}
+
+variable "app_count" {
+  default     = "3" 
+  description = "numer of docker containers to run"
+}
+
+variable "health_check_path" {
+  default = "/"
+}
+
+variable "fargate_cpu" {
+  default     = "512"
+  description = "Fargate instacne CPU units to provision"
+}
+
+variable "fargate_memory" {
+  default     = "1024"
+  description = "Fargate instance memory to provision (in MiB) not MB"
 }
