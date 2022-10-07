64a622f5c92e2a9e9f78f873c674a5847e563295
jhflo <jorgeflorez28@hotmail.com>
2022-10-07 10:06:13 -0500

    Create secutiry.tf

---
+++ secutiry.tf
@@ -0,0 +1,40 @@
+resource "aws_security_group" "alb-sg" {
+  name        = "testapp-load-balancer-security-group"
+  description = "controls access to the ALB"
+  vpc_id      = aws_vpc.test-vpc.id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = var.app_port
+    to_port     = var.app_port
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# ECS security group 
+resource "aws_security_group" "ecs_sg" {
+  name        = "testapp-ecs-tasks-security-group"
+  description = "Allow inbound access from the ALB only"
+  vpc_id      = aws_vpc.test-vpc.id
+
+  ingress {
+    protocol        = "tcp"
+    from_port       = var.app_port
+    to_port         = var.app_port
+    security_groups = [aws_security_group.alb-sg.id]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
